f5c9f46e7a4b77816c3c24295cb1a6fd0a615fc6
tay7ay <tay7ay@gmail.com>
2023-04-25 10:43:58 -0400

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 const form = document.querySelector("form");
 eField = form.querySelector(".userID"),
 eInput = eField.querySelector("input"),
-pField = form.querySelector(".password"),
-pInput = pField.querySelector("input");
 
 form.onsubmit = (e)=>{
   e.preventDefault();
   (eInput.value == "") ? eField.classList.add("shake", "error") : checkID();
-  (pInput.value == "") ? pField.classList.add("shake", "error") : checkPass();
 
   setTimeout(()=>{ 
     eField.classList.remove("shake");
-    pField.classList.remove("shake");
   }, 500);
 
   eInput.onkeyup = ()=>{checkID();} 
-  pInput.onkeyup = ()=>{checkPass();} 
 
   function checkID(){
     let pattern = /^\d{9}$/;
@@ -30,24 +25,14 @@ form.onsubmit = (e)=>{
     }
   }
 
-  function checkPass(){
-    if(pInput.value == ""){
-      pField.classList.add("error");
-      pField.classList.remove("valid");
-    }else{
-      pField.classList.remove("error");
-      pField.classList.add("valid");
-    }
-  }
-
-  if(!eField.classList.contains("error") && !pField.classList.contains("error")){
+  if(!eField.classList.contains("error")){
     window.location.href = form.getAttribute("action");
     
     if(eInput.value == "913236543"){
-      alert("Welcome Tayvion!")
+      alert("Welcome Tayvion Lyons!")
     }
     if(eInput.value == "913111111"){
-      alert("Welcome Jeremiah!")
+      alert("Welcome Jeremiah Harrison!")
     }
   }
 }
